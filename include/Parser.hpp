78ecebeef9ed87174a6325214c40c388350f488d
Andi Doty <andi.j.doty@gmail.com>
2022-09-12 21:17:15 -0500

    Parser Class and Function Prototypes
    Added the base class and function prototypes for the Parser class.
    
    To Do Next:
    - Add print statement functionality.
    - Add EOL functionality.
    - Add EOF functionality (?).

---
+++ include/Parser.hpp
@@ -0,0 +1,25 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include "Token.hpp"
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Parser
+{
+    public:
+
+        // Class Variables.
+        vector<Token> tokens;
+
+        // Class Constructor.
+        Parser(vector<Token> tokens);
+        
+        // Class Functions.
+        bool parse();
+};
+
+#endif
---
+++ src/Parser.cpp
@@ -0,0 +1,11 @@
+#include "../include/Parser.hpp"
+
+Parser::Parser(vector<Token> tokens)
+{
+    this->tokens = tokens;
+}
+
+bool Parser::parse()
+{
+    return true;
+}
